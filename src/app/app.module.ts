ddf83c8dba522157e57d30f8f86327102056842c
SechabaRapeana <39264664+SechabaRapeana@users.noreply.github.com>
2018-06-19 13:53:38 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { InstallComponent } from './install/install.component';
+import { StartComponent } from './start/start.component';
+import { RunComponent } from './run/run.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent,InstallComponent,StartComponent,RunComponent, HomeComponent],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'home', component:  HomeComponent},
+      {path: 'install', component:  InstallComponent},
+      {path: 'start', component:  StartComponent},
+      {path: 'run', component:  RunComponent},
+     
+    ])],
   providers: [],
   bootstrap: [AppComponent]
 })
